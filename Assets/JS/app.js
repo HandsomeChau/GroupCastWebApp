f8a04f4dbabd21d3527afc44abec6793c332b303
Sean Hodgies <sean.hodgies@gmail.com>
2015-04-05 17:58:11 -0700

    Inital commit once again. This time for real.

---
+++ Assets/JS/app.js
@@ -0,0 +1,46 @@
+﻿(function ()
+{
+    var app = angular.module('w11', ["ngRoute"]);
+
+    app.config(function ($routeProvider)
+    {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "/Views/main.html",
+                controller: "myController"
+            })
+            .when("/login", {
+                templateUrl: "/Views/Account/login.html",
+                controller: "myController"
+            })
+            .when("/forgotPassword", {
+                templateUrl: "/Views/Account/forgotPassword.html",
+                controller: "myController"
+            })
+            .when("/sendEmail", {
+                templateUrl: "/Views/Account/sendEmail.html",
+                controller: "myController"
+            })
+            .when("/register", {
+                templateUrl: "/Views/Account/register.html",
+                controller: "myController"
+            })
+            .when("/dashboard", {//May not use
+                templateUrl: "/Views/dashboard.html",
+                controller: "myController"
+            })
+            .when("/group", {
+                templateUrl: "/Views/group.html",
+                controller: "myController"
+            })
+            .when("/sendMessage", {
+                templateUrl: "/Views/sendMessage.html",
+                controller: "myController"
+            })
+            .when("/receiveMessage", {
+                templateUrl: "/Views/receiveMessage.html",
+                controller: "myController"
+            })
+            .otherwise({redirectTo: "/main"});
+    });
+}());
---
+++ Assets/JS/myController.js
@@ -0,0 +1,60 @@
+﻿(function ()
+{
+    var app = angular.module("w11");
+    var baseUrl = "http://w01a.code8080.com/api/";
+
+    var myController = function ($scope, $http)
+    {
+        $scope.categories_create = function ()
+        {
+            var name = $scope.cat_name;
+            var description = $scope.cat_description
+            var data = JSON.stringify({CategoryName: name, Description: description});
+            $http.post(baseUrl + "Categories", data)
+                .success(function (response)
+                {
+                    $scope.response = "Created Category " + name;
+                }
+            );
+        }
+
+        $scope.categories_delete = function ()
+        {
+            var id = $scope.cat_id;
+            $http.delete(baseUrl + "Categories/" + id)
+                .success(function (response)
+                {
+                    $scope.response = "Deleted Category " + id;
+                }
+            );
+        }
+
+        $scope.categories_displayAll = function ()
+        {
+            $http.get(baseUrl + "Categories")
+                .success(function (response)
+                {
+                    $scope.categoryInfo = response;
+                }
+            );
+        }
+
+        $scope.categories_update = function ()
+        {
+
+            var id = $scope.cat_id;
+            var name = $scope.cat_name;
+            var description = $scope.cat_description
+            var data = JSON.stringify({CategoryID: id, CategoryName: name, Description: description});
+            $http.put(baseUrl + "Categories/" + id, data)
+                .success(function (response)
+                {
+                    $scope.response = "Updated Category " + id;
+                }
+            );
+        }
+    };
+
+    app.controller("myController", ["$scope", "$http", "$routeParams", myController]);
+}());
+
