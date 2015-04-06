5e4ebb03ae7b1cb3baabe2525a9d4f6dc73a0744
Sean Hodgies <sean.hodgies@gmail.com>
2015-04-06 14:52:39 -0700

    Added all the basic views, need the API to really continue

--- Assets/JS/myController.js
+++ Assets/JS/myController.js
@@ -1,13 +1,13 @@
 ﻿(function ()
 {
     var app = angular.module("w11");
-    var baseUrl = "http://w01a.code8080.com/api/";
+    var baseUrl = "http://groupcast.andrewhartline.com/api/";
 
     var myController = function ($scope, $http)
     {
         $scope.categories_displayAll_test = function ()
         {
-            $http.get(baseUrl + "Categories")
+            $http.get(baseUrl + "")
                 .success(function (response)
                 {
                     $scope.categoryInfo = response;
@@ -15,7 +15,21 @@
             );
         }
 
+        $scope.getAllStudentCourses = function ()
+        {
+            $http.get(baseUrl = "")
+                .success(function (response)
+                {
+                    $scope.courses = response;
+                }
+            );
+        }
 
+        $scope.sendToCheckCourses = function ()
+        {
+            var checkedCourses = $scope.null()
+            alert("Courses Received TEST");
+        }
 
 
     };
